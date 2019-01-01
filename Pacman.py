cde612af16fcb7edf148383dca1d5abf01920030
aharonlouzon <46280543+aharonlouzon@users.noreply.github.com>
2019-01-01 15:18:40 +0200

    Update Pacman.py

--- Pacman.py
+++ Pacman.py
@@ -114,21 +114,3 @@ class Board:
                     sub_node["state"] = move
                     sub_node["cost"] = node["cost"] + 1
                     frontier.push(sub_node, sub_node["cost"])
-
-
-# Example1:
-b = Board(10, 10)
-b.putPacman([3, 4])
-b.putGhost([[3, 5], [1, 7], [6, 6]])
-b.putWalls([[4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9]])
-print("\n")
-print(b.board)
-b.findGhosts()
-
-# Example2:
-b = Board(10, 10)
-b.putPacman([3, 4])
-b.randomize()
-print("\n")
-print(b.board)
-b.findGhosts()
