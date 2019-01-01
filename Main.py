51cc7e98a444673c160520489715f6ebd2059ba5
aharonlouzon <46280543+aharonlouzon@users.noreply.github.com>
2019-01-01 15:35:31 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,17 @@
 import Pacman
 
-def main():
+# Example1:
+b = Pacman.Board(10, 10)
+b.putPacman([3, 4])
+b.putGhost([[3, 5], [1, 7], [6, 6]])
+b.putWalls([[4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9]])
+print(b.board)
+b.findGhosts()
 
-    # Example1:
-    b = Pacman.Board(10, 10)
-    b.putPacman([3, 4])
-    b.putGhost([[3, 5], [1, 7], [6, 6]])
-    b.putWalls([[4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9]])
-    print("\n")
-    print(b.board)
-    b.findGhosts()
-
-    # Example2:
-    b = Pacman.Board(10, 10)
-    b.putPacman([3, 4])
-    b.randomize()
-    print("\n")
-    print(b.board)
-    b.findGhosts()
+# Example2:
+b = Pacman.Board(10, 10)
+b.putPacman([3, 4])
+b.randomize()
+print("\n")
+print(b.board)
+b.findGhosts()
