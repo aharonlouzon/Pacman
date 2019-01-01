cb7f3c2b0c0e4b01a99fd8f07366044ca3cabc90
aharonlouzon <46280543+aharonlouzon@users.noreply.github.com>
2019-01-01 17:47:51 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,9 +4,10 @@ import Pacman
 b = Pacman.Board(10, 10)
 b.putPacman([3, 4])
 b.putGhost([[3, 5], [1, 7], [6, 6]])
-b.putWalls([[4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9]])
+b.putWalls([[4, 1], [4, 2], [4, 3], [4, 4], [4, 5], [4, 6], [4, 7], [4, 8], [4, 9]])
 print(b.board)
 b.findGhosts()
+print("\n")
 b.findGhostsHeuristic()
 
 # Example2:
@@ -16,4 +17,5 @@ b.randomize()
 print("\n")
 print(b.board)
 b.findGhosts()
+print("\n")
 b.findGhostsHeuristic()
